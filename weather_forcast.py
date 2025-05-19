9d26b788d0c8a928c1965997c912a44d9bd7783d
Lonpsy <oolalonpe@gmail.com>
2025-05-19 15:02:48 +0200

    updated forcast app to more user friendly interface

--- weather_forcast.py
+++ weather_forcast.py
@@ -1,31 +1,34 @@
 from rich import print
 import requests
+from datetime import datetime
 
 
-def greetings():
-  greetings = print("make sure you dress properly")
+def advice():
+    advice = print("remember to dress properly")
+    print(advice)
 
 
 def forcast_weather(city):
-  days = ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat']
-  api_key = 'da7a1b3d460dbtbf7b304o1bb99604f1'
-  api_url = f'https://api.shecodes.io/weather/v1/forecast?query={city}&key={api_key}'
-  index = 0
-
-  for day in days:
-
+    api_key = 'da7a1b3d460dbtbf7b304o1bb99604f1'
+    api_url = f'https://api.shecodes.io/weather/v1/forecast?query={city}&key={api_key}'
+    index = 0
     response = requests.get(api_url)
     forcast = response.json()
-    weather_forcast_minimum = round(
-        forcast['daily'][index]['temperature']['minimum'])
-    weather_forcast_maximum = round(
-        forcast['daily'][index]['temperature']['maximum'])
-    print(
-        f"{day} : min:{weather_forcast_minimum} max:{weather_forcast_maximum}")
-    index = index + 1
+    for day in forcast['daily']:
+        timestamp = day['time']
+        formated_date = datetime.fromtimestamp(timestamp)
+        formated_day = formated_date.strftime('%A')
+        today = round(day['temperature']['day'])
+        weather_forcast_minimum = round(day['temperature']['minimum'])
+        weather_forcast_maximum = round(day['temperature']['maximum'])
+        print(f'{formated_day}')
+        print(
+            f"{today} : [purple bold]  minmum[/purple bold]:{weather_forcast_minimum}  maximum {weather_forcast_maximum}"
+        )
+        index = index + 1
 
 
 city = input("enter your current city ")
 forcast_weather(city)
-
-greetings()
+print('\n')
+advice()
