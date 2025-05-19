332c0365c7cbdf9a7bae17dfbe6458d6cf9717d4
Lonpsy <oolalonpe@gmail.com>
2025-05-19 15:47:26 +0200

    included current weather in the python weather app

--- weather_forcast.py
+++ weather_forcast.py
@@ -8,6 +8,15 @@ def advice():
     print(advice)
 
 
+def current_weather(city):
+    api_key = 'da7a1b3d460dbtbf7b304o1bb99604f1'
+    api_url = f'https://api.shecodes.io/weather/v1/current?query={city}&key={api_key}'
+    response = requests.get(api_url)
+    weather_data = response.json()
+    today_weather = round(weather_data['temperature']['current'])
+    print(f'It is currently {today_weather}°C in {city}')
+
+
 def forcast_weather(city):
     api_key = 'da7a1b3d460dbtbf7b304o1bb99604f1'
     api_url = f'https://api.shecodes.io/weather/v1/forecast?query={city}&key={api_key}'
@@ -21,14 +30,19 @@ def forcast_weather(city):
         today = round(day['temperature']['day'])
         weather_forcast_minimum = round(day['temperature']['minimum'])
         weather_forcast_maximum = round(day['temperature']['maximum'])
-        print(f'{formated_day}')
-        print(
-            f"{today} : [purple bold]  minmum[/purple bold]:{weather_forcast_minimum}  maximum {weather_forcast_maximum}"
-        )
+
+        if formated_date.date() != datetime.today().date():
+            print(f'{formated_day}')
+            print(
+                f"[purple bold]  minmum[/purple bold]:{weather_forcast_minimum}  maximum {weather_forcast_maximum}"
+            )
         index = index + 1
 
 
 city = input("enter your current city ")
+print('\n')
+current_weather(city)
+print('\n')
 forcast_weather(city)
 print('\n')
 advice()
